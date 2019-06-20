917d29379db88580e5116fe4a65be8fd7e24ca1a
AJ <alainrjoseph@gmail.com>
2019-06-19 20:02:22 -0400

    cameraFunctions

---
+++ camera/Lightbox.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
+
+const images = [
+  "//firebase.com/pic1",
+  "//firebase.com/pic2",
+  "//firebase.com/pic3",
+  "//firebase.com/pic4"
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photoIndex: 0,
+      isOpen: false
+    };
+  }
+
+  render() {
+    const { photoIndex, isOpen } = this.state;
+
+    return (
+      <div>
+        <button type="button" onClick={() => this.setState({ isOpen: true })}>
+          Open Lightbox
+        </button>
+
+        {isOpen && (
+          <Lightbox
+            mainSrc={images[photoIndex]}
+            nextSrc={images[(photoIndex + 1) % images.length]}
+            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + images.length - 1) % images.length
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % images.length
+              })
+            }
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
