87cf3f0f43e6a7afcad025f0121f184eddae061d
Dowan Kim <dowankim@Dowanui-MacBook-Pro.local>
2019-06-19 20:09:39 -0400

    more work on frame

--- client/src/components/Calender/Calender.js
+++ client/src/components/Calender/Calender.js
@@ -3,9 +3,9 @@ import "./Calender.css";
 
 function Calender() {
     return (
-        <div class="container">
-            <h2>Daily Calories Calender</h2>
-            <div class="row">
+        <div class="container" id="dailyCalender">
+            <h2 id="clientPageTitle">Daily Calories Calender</h2>
+            <div class="row" id="calenderContainer">
                 <div class="col-md-1" id="sun">Sun</div>
                 <div class="col-md-1" id="sun">Mon</div>
                 <div class="col-md-1" id="sun">Tue</div>
@@ -14,28 +14,37 @@ function Calender() {
                 <div class="col-md-1" id="sun">Fri</div>
                 <div class="col-md-1" id="sun">Sat</div>
             </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="input-group mb-3">
+            <div class="row" id="calenderContainer">
+                <div class="col-md-5" id="containerBorder">
+                    <div class="input-group mb-3" id="searchSection">
                         <input type="text" class="form-control" placeholder="Calories Search by Food" aria-label="Calories Search by Food" aria-describedby="button-addon2"/>
                         <div class="input-group-append">
                             <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
                         </div>
                     </div>
-                    <br/>
-                    <div class="input-group mb-3">
+                    <div class="input-group mb-3" id="searchSection">
                         <input type="text" class="form-control" placeholder="Calories Search by Restaurant Menu" aria-label="Calories Search by Restaurant Menu" aria-describedby="button-addon2"/>
                         <div class="input-group-append">
                             <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <span>Breakfast : </span>
-                    <br/>
-                    <span>Lunch : </span>
-                    <br/>
-                    <span>Dinner : </span>
+                <div class="col-md-7" id="containerBorder">
+                    <div id="calenderContent">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    Breakfast : 
+                                </div>
+                                <div class="col-md-4">
+                                    Lunch :
+                                </div>
+                                <div class="col-md-4">
+                                    Dinner : 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div>
