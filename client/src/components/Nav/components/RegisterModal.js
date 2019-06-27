ac75321582bc3f6a88402dcbf85271b686d947cc
Camilo Agudelo <camilo@Camilos-MacBook-Pro-2.local>
2019-06-27 17:56:35 -0400

    Version working with last saras commit and Dowan design by 062719 555 pm

--- client/src/components/Nav/components/RegisterModal.js
+++ client/src/components/Nav/components/RegisterModal.js
@@ -38,44 +38,42 @@ class RegisterModal extends React.Component {
     render() {
 
         return (
-                <div className="modal fade" role="dialog" id="registerModal">
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h3 className="modal-title">Create a YOLO account</h3>
-                                <button type="button" className="close" data-dismiss="modal">&times;</button>
+            <div className="modal fade" role="dialog" id="registerModal">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h3 className="modal-title">Create a YOLO account</h3>
+                            <button type="button" className="close" data-dismiss="modal">&times;</button>
+                        </div>
+
+                        <div className="modal-body">
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="">Last and First Name</span>
+                                </div>
+                                <input type="text" className="form-control" placeholder="Last Name" name="lastName" onChange={this.handleInputChange} />
+                                <input type="text" className="form-control" placeholder="First Name" name="firstName" onChange={this.handleInputChange} />
+                            </div>
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="inputGroup-sizing-default">Email</span>
+                                </div>
+                                <input type="text" className="form-control" aria-label="Email" aria-describedby="inputGroup-sizing-default" placeholder="Email Address" name="email" onChange={this.handleInputChange} />
+                            </div>
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="inputGroup-sizing-default">Password</span>
+                                </div>
+                                <input type="password" className="form-control" aria-label="Password" aria-describedby="inputGroup-sizing-default" placeholder="Password" name="password" onChange={this.handleInputChange} />
                             </div>
 
-                            <div className="modal-body">
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="">Last and First Name</span>
-                                    </div>
-                                    <input type="text" className="form-control" placeholder="Last Name" name="lastName" onChange={this.handleInputChange} />
-                                    <input type="text" className="form-control" placeholder="First Name" name="firstName" onChange={this.handleInputChange} />
-                                </div>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="inputGroup-sizing-default">Email</span>
-                                    </div>
-                                    <input type="text" className="form-control" aria-label="Email" aria-describedby="inputGroup-sizing-default" placeholder="Email Address" name="email" onChange={this.handleInputChange} />
-                                </div>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="inputGroup-sizing-default">Password</span>
-                                    </div>
-                                    <input type="password" className="form-control" aria-label="Password" aria-describedby="inputGroup-sizing-default" placeholder="Password" name="password" onChange={this.handleInputChange} />
-                                </div>
 
-
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="inputGroup-sizing-default">Age</span>
-                                    </div>
-                                    <input type="text" className="form-control" aria-label="Age" aria-describedby="inputGroup-sizing-default" placeholder="Age" name="age" onChange={this.handleInputChange} />
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="inputGroup-sizing-default">Age</span>
                                 </div>
-
                                 <input type="text" className="form-control" aria-label="Age" aria-describedby="inputGroup-sizing-default" placeholder="Age" name="age" onChange={this.handleInputChange} />
+                                {/* <input type="text" className="form-control" aria-label="Age" aria-describedby="inputGroup-sizing-default" placeholder="Age" name="age" onChange={this.handleInputChange} /> */}
                                 <div class="input-group-prepend">
                                     <label class="input-group-text" for="inputGroupSelect01">Gender</label>
                                 </div>
@@ -85,30 +83,29 @@ class RegisterModal extends React.Component {
                                     <option value="f">Female</option>
                                 </select>
                             </div>
-                            <div className="input-group mb-3">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text" id="inputGroup-sizing-default">Height</span>
-                                </div>
-                                <input type="text" className="form-control" aria-label="HeightF" aria-describedby="inputGroup-sizing-default" placeholder="Feet" name="heightFeet" onChange={this.handleInputChange} />
-                                <input type="text" className="form-control" aria-label="HeightI" aria-describedby="inputGroup-sizing-default" placeholder="Inches" name="heightInches" onChange={this.handleInputChange} />
-
-
-                                </div>
-                                <div className="input-group mb-3">
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="inputGroup-sizing-default">Weight</span>
-                                    </div>
-                                    <input type="text" className="form-control" aria-label="Weight" aria-describedby="inputGroup-sizing-default" placeholder="Height  Pounds" name="weight" onChange={this.handleInputChange} />
-                                </div>
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">Height</span>
                             </div>
+                            <input type="text" className="form-control" aria-label="HeightF" aria-describedby="inputGroup-sizing-default" placeholder="Feet" name="heightFeet" onChange={this.handleInputChange} />
+                            <input type="text" className="form-control" aria-label="HeightI" aria-describedby="inputGroup-sizing-default" placeholder="Inches" name="heightInches" onChange={this.handleInputChange} />
 
-                            <div className="modal-footer">
-                                <button onClick={this.registerUser} className="btn btn-sucess">Create an Account</button>
+
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">Weight</span>
                             </div>
+                            <input type="text" className="form-control" aria-label="Weight" aria-describedby="inputGroup-sizing-default" placeholder="Height  Pounds" name="weight" onChange={this.handleInputChange} />
+                        </div>
+                        <div className="modal-footer">
+                            <button onClick={this.registerUser} className="btn btn-sucess">Create an Account</button>
                         </div>
                     </div>
-                </div>
 
+                </div>
+            </div>
         )
     }
 }
